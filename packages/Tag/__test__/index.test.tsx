746f174e814b96e49d412c46f9268d1b2d2e03ea
suemor <suemor233@outlook.com>
2023-06-10 22:44:29 +0800

    chore: remove unused import

--- packages/Tag/__test__/index.test.tsx
+++ packages/Tag/__test__/index.test.tsx
@@ -1,6 +1,6 @@
 import { describe, expect, it, vi } from 'vitest'
 
-import { fireEvent, render, screen } from '@testing-library/react'
+import { fireEvent, render } from '@testing-library/react'
 
 import { Tag, TagProps } from '../..'
 
